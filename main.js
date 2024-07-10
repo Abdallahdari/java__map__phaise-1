1070a15d0722ddce7b498f3f656e5cd6daf2ff6b
ABDULLAHI ABDIRIZAK MOHAMED <105462733+Abdallahdari@users.noreply.github.com>
2024-07-10 22:15:34 +0300

    the map is working but the left border is not showing

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-navigator.geolocation.getCurrentPosition(function(position){
-const {latitude}=position.coords
-const {longitude}=position.coords
-console.log(latitude,longitude) ;
+document.addEventListener('DOMContentLoaded', function () {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const { latitude, longitude } = position.coords;
+        const coord = [latitude, longitude];
+        const map = L.map('map').setView(coord, 13);
 
+        L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
+        map.on('click', function (e) {
+            const { lat, lng } = e.latlng;
+            L.marker([lat, lng]).addTo(map)
+                .bindPopup(L.popup({
+                    maxWidth: 250,
+                    minWidth: 200,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'workout'
+                }))
+                .setPopupContent("Workout")
+                .openPopup();
+        });
 
-
-},function(){
-    alert("your location couldnot findit")
-})
+    }, function () {
+        alert("Your location could not be found");
+    });
+});
